b506e95ae97813a18d398cf41aae7896c0503550
Mike <wenhongyu1996@qq.com>
2019-02-28 20:11:39 +0800

    update

---
+++ 1.4.py
@@ -0,0 +1,7 @@
+hb=input()
+if hb[0:3] in ['RMB']:
+    USD=eval(hb[3:])/6.78
+    print("USD{:.2f}".format(USD))
+elif hb[0:3] in ['USD']:
+    RMB=eval(hb[3:])*6.78
+    print("RMB{:.2f}".format(RMB))
