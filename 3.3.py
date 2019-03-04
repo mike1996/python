3e8f8d019ed0f77f120beb843dfd358b60e832cd
Mike <wenhongyu1996@qq.com>
2019-03-04 17:44:37 +0800

    update

--- 3.3.py
+++ 3.3.py
@@ -3,7 +3,7 @@ dayup = 1.0
 dayfactor = 0.01
 for i in range(365):
     if i % 7 in [6, 0]:
-        dayup = dayup*(1-dayfactor)
+        dayup = dayup*(1)
     else:
         dayup = dayup*(1+dayfactor)
 print("dayup:{:0.2f},".format(dayup))
