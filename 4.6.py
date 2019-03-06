d364a37d96529073a9afc1798cdac5a371c02218
Mike <wenhongyu1996@qq.com>
2019-03-06 18:39:53 +0800

    update

--- 4.6.py
+++ 4.6.py
@@ -3,9 +3,9 @@ while <>:
     <>
 中文测试
 '''
-a=3
-while a>0:
+# %%
+a = 3
+while a > 0:
     print(a)
-    a=a-1
+    a = a-1
 print("测试中文")
-
--- 4.7.py
+++ 4.7.py
@@ -1,4 +1,41 @@
+# 循环
+# %% continue
 for c in "PYTHON":
     if c == "T":
         continue
     print(c, end="")
+# %% break
+for c in "PYTHON":
+    if c == "T":
+        break
+    print(c, end="")
+# %%
+s = "PYTHON"
+while s != "":
+    for c in s:
+        print(c, end="")
+    s = s[:-1]
+# %% break仅仅能退出最内层
+s = "PYTHON"
+while s != "":
+    for c in s:
+        if c == "T":
+            break
+        print(c, end="")
+    s = s[:-1]
+# %%
+s = "PYTHON"
+while s != "":
+    for c in s:
+        if c == "T":
+            continue
+        print(c, end="")
+    s = s[:-1]
+# %% 循环与else
+# 当循环没有被break推出时,执行else
+for c in "PYTHON":
+    if c == "T":
+        continue
+    print(c, end="")
+else:
+    print("\n正常退出")
